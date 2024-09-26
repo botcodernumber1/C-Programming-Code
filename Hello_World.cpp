8d20f6f3479c83e0717ed5dd5ec0dbd97cbadf2f
botcodernumber1 <khairulislam7879@gmail.com>
2024-09-26 15:52:02 +0600

    #Third Comment

--- Hello_World.cpp
+++ Hello_World.cpp
@@ -1,8 +1,10 @@
-#include <iostream>//For Predefined Function Iostream Means Input & Output This Called Header File Such As Cin = Input / Cout = Output
+#include <iostream>// Includes the Input/Output stream library for cin (input) and cout (output) For Predefined Function Iostream Means Input & Output This Called Header File Such As Cin = Input / Cout = Output
 
 //using namespace std;// STD stands for Standard Namespace
 
 int main()
 {
-    std::cout<<"Hello World!";
+    // Using std::cout to print "Hello World!" to the console
+    std::cout << "Hello World!" << std::endl; // std::endl moves the cursor to a new line
+    return 0; // Return 0 to indicate the program executed successfully
 }
